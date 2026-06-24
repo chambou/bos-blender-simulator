b789d063c2b83595d0d88b71751c8f601b0e5c24
Bryan Miguel <tulaobryanmiguel@gmail.com>
2026-06-24 17:21:32 +0200

    add padding for cross correlation in process_stereoscopy

--- process_stereoscopy.py
+++ process_stereoscopy.py
@@ -131,17 +131,44 @@ def correlation_1d(img_left, img_right, kernel_center, kernel_width):
     # create left image patch
     patch_L = img_left[y1:y2, x1:x2].astype(np.float32)
 
-    # Search for best match in right image (constrained by baseline)
+    # # Search for best match in right image (constrained by baseline)
+    # # For parallel stereo, extrema should be at similar y, but different x (disparity)
+    # search_x_min = half_win_width
+    # search_x_max = img_right.shape[1] - half_win_width  # Right image should be to the left or same x
+
+    # axis = []
+    # for x_R in range(search_x_min, search_x_max):
+    #     x1_R = x_R - half_win_width
+    #     x2_R = x1_R + kernel_width
+        
+    #     patch_R = img_right[y1:y2, x1_R:x2_R].astype(np.float32)
+        
+    #     # Normalized cross-correlation (only if patches are same size and nonzero)
+    #     if patch_L.shape == patch_R.shape and patch_L.std() > 0 and patch_R.std() > 0:
+    #         corr = np.corrcoef(patch_L.ravel(), patch_R.ravel())[0, 1]
+    #         correlation.append(corr)
+    #         if corr > best_corr:
+    #             best_corr = corr
+    #             best_x_R = x_R
+    #     axis.append(x_R)
+
+    # Search for best match in right image (constrained by baseline, with padding)
     # For parallel stereo, extrema should be at similar y, but different x (disparity)
     search_x_min = half_win_width
-    search_x_max = img_right.shape[1] - half_win_width  # Right image should be to the left or same x
+    search_x_max = img_right.shape[1] + half_win_width  # Right image should be to the left or same x
+
+    # padding
+    img_right_padded = np.zeros((img_right.shape[0], img_right.shape[1] + 2*half_win_width), dtype=img_left.dtype)
+    img_right_padded[:, half_win_width:half_win_width + img_right.shape[1]] = img_right
+    print(img_right_padded.shape)
 
     axis = []
     for x_R in range(search_x_min, search_x_max):
+        x_R_orig = x_R - half_win_width
         x1_R = x_R - half_win_width
         x2_R = x1_R + kernel_width
         
-        patch_R = img_right[y1:y2, x1_R:x2_R].astype(np.float32)
+        patch_R = img_right_padded[y1:y2, x1_R:x2_R].astype(np.float32)
         
         # Normalized cross-correlation (only if patches are same size and nonzero)
         if patch_L.shape == patch_R.shape and patch_L.std() > 0 and patch_R.std() > 0:
@@ -149,8 +176,8 @@ def correlation_1d(img_left, img_right, kernel_center, kernel_width):
             correlation.append(corr)
             if corr > best_corr:
                 best_corr = corr
-                best_x_R = x_R
-        axis.append(x_R)
+                best_x_R = x_R_orig
+        axis.append(x_R_orig)
 
     return axis, correlation, best_x_R
 
@@ -175,7 +202,7 @@ extrema_candidates_L = np.argwhere(local_max | local_min)
 # Suppress nearby ones
 extrema_L = suppress_nearby_extrema(extrema_candidates_L, img_left, min_distance=min_distance_NMS)
 
-print(f"After filtering: {len(extrema_L)} extrema in the left image")
+# print(f"After filtering: {len(extrema_L)} extrema in the left image")
 
 # --- Match extrema across images using cross-correlation ---
 # window_size = 501                   # size of the local patch around extrema [px]
@@ -185,16 +212,15 @@ print(f"After filtering: {len(extrema_L)} extrema in the left image")
 
 # print(f"Matched {len(matches)} extrema pairs")
 
-# 1-dimentional correlation
-center = (img_left.shape[1]//2) + 300
-width = img_left.shape[0]
-ax, corr_1d, best_x_R = correlation_1d(img_left, img_right, center, width)
+# --- 1-dimentional correlation ---
+center = (img_left.shape[1]//2) + 400
+width = img_left.shape[0] // 2
+#ax, corr_1d_8bit, best_x_R_8bit = correlation_1d(img_left, img_right, center, width)
+# without 8-bit conversion
+ax, corr_1d, best_x_R = correlation_1d(cam0, cam1, center, width)
 
+#disp_8bit = center - best_x_R_8bit
 disp = center - best_x_R
-
-plt.figure()
-plt.plot(ax, corr_1d)
-plt.show()
 
 
 # --- Compute depth for each match ---
@@ -208,8 +234,55 @@ sensor_mm = config["camera"]["sensor_size"]
 W_px = config["camera"]["resolution_x"]
 
 f_px = (f_mm / sensor_mm) * W_px
+Z = (f_px * B) / disp
 
-print("depth: ", (f_px * B) / disp)
+#print("depth with 8 bit image: ", (f_px * B) / disp_8bit)
+print("depth with original image: ", (f_px * B) / disp)
+
+
+# plt.figure()
+# plt.plot(ax, corr_1d)
+# plt.title('Image Correlation')
+# plt.xlim(0, img_right.shape[1]); plt.ylim(0, 1)
+# plt.show()
+
+stats_text = (
+    "At best correlation:\n"
+    f"Disparity: {disp} px\n"
+    f"Depth:   {Z:.3f} m\n"
+)
+
+fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(14, 6))
+im0 = ax0.imshow(cam0, cmap='viridis')
+ax0.set_title('Left image')
+ax0.set_xlim(0, img_left.shape[1]); ax0.set_ylim(img_left.shape[0], 0)
+cbar0 = plt.colorbar(im0, ax=ax0)
+cbar0.set_label('Intensity', rotation=270, labelpad=15)
+
+im1 = ax1.imshow(cam1, cmap='viridis')
+ax1.set_title('Right image')
+ax1.set_xlim(0, img_right.shape[1]); ax1.set_ylim(img_right.shape[0], 0)
+cbar0 = plt.colorbar(im1, ax=ax1)
+cbar0.set_label('Intensity', rotation=270, labelpad=15)
+
+plt.tight_layout()
+plt.show(block=False)
+fig.savefig(os.path.join(output_folder,'stereo.png'), dpi=fig.dpi)
+
+
+plt.figure()
+plt.plot(ax, corr_1d)
+plt.title('Image Correlation')
+plt.xlim(0, img_right.shape[1]); plt.ylim(0, 1)
+plt.xlabel("px")
+plt.text(
+    1350, 0.05, stats_text,
+    fontsize=10,
+    color='white',
+    bbox=dict(facecolor='black', alpha=0.5, edgecolor='none')
+)
+plt.show()
+
 
 # # Compute depth for each match
 # depths = []
